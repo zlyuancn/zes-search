ea8973682c977116b3890238830ff20682aa578a
zlyuancn <zlyuancn@163.com>
2021-06-04 17:53:18 +0800

    解析innerHits时在搜索字段的检查优先级为 inner_hits标签 > json标签 > 字段名完全相等

--- search.go
+++ search.go
@@ -20,7 +20,10 @@ import (
 )
 
 // 解析innerHits的标签名
-const InnerHitsTagName = "json"
+const (
+	InnerHitsTagName       = "inner_hits"
+	InnerHitsSecondTagName = "json" // 第二优先级
+)
 
 var ErrNoDocuments = errors.New("没有匹配的结果")
 
@@ -253,7 +256,7 @@ func parseInnerHit(inner_key string, innerHits *elastic.SearchHitInnerHits, item
 		item_value.SetMapIndex(reflect.ValueOf(inner_key), child_value)
 	case reflect.Struct:
 		// 获取结构体的指定字段
-		field_value, field_type, ok := searchStructField(item_value, item_type, inner_key)
+		field_value, field_type, ok := searchStructFieldWithInnerHit(item_value, item_type, inner_key)
 		if !ok {
 			return nil
 		}
@@ -285,21 +288,41 @@ func parseInnerHit(inner_key string, innerHits *elastic.SearchHitInnerHits, item
 	return nil
 }
 
-// 搜索结构体字段, 返回(字段反射值,字段类型,是否找到)
-func searchStructField(item_value reflect.Value, item_type reflect.Type, key string) (reflect.Value, reflect.Type, bool) {
-	field_num := item_type.NumField()
-	for i := 0; i < field_num; i++ {
-		field := item_type.Field(i)
-		// 忽略未导出字段
-		if field.PkgPath != "" {
-			continue
+// 搜索结构体字段用于innerHits, 首选tag > 次选tag > 名称相等, 返回(字段反射值,字段类型,是否找到)
+func searchStructFieldWithInnerHit(item_value reflect.Value, item_type reflect.Type, key string) (out_value reflect.Value, out_type reflect.Type, ok bool) {
+	// 搜索函数, 根据比较函数返回并设置结果
+	searchFn := func(compareFn func(field *reflect.StructField) bool) bool {
+		field_num := item_type.NumField()
+		for i := 0; i < field_num; i++ {
+			field := item_type.Field(i)
+			// 忽略未导出字段
+			if field.PkgPath != "" {
+				continue
+			}
+
+			// 检查
+			if compareFn(&field) {
+				out_value, out_type, ok = item_value.Field(i), field.Type, true
+				return true
+			}
 		}
-		// 如果标签名复合key或者字段名等于key
-		if field.Tag.Get(InnerHitsTagName) == key || field.Name == key {
-			return item_value.Field(i), field.Type, true
+		return false
+	}
+
+	compareFns := []func(field *reflect.StructField) bool{
+		// 搜索首选tag
+		func(field *reflect.StructField) bool { return field.Tag.Get(InnerHitsTagName) == key },
+		// 搜索次选tag
+		func(field *reflect.StructField) bool { return field.Tag.Get(InnerHitsSecondTagName) == key },
+		// 检查名称等于key
+		func(field *reflect.StructField) bool { return field.Name == key },
+	}
+	for _, compareFn := range compareFns {
+		if searchFn(compareFn) {
+			return
 		}
 	}
-	return reflect.Value{}, nil, false
+	return
 }
 
 // 搜索获取总数
